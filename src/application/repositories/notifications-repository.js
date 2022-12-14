8c855fa88260d6f4739a99dc048d7a892ef4cad3
Jones Kleber <visao@visao-ol.com.br>
2022-12-14 15:40:36 -0300

    Second class

---
+++ src/application/repositories/notifications-repository.js
@@ -0,0 +1,10 @@
+"use strict";
+// é apenas o contrato e não implementação dos métodos
+exports.__esModule = true;
+exports.NotificationsRepository = void 0;
+var NotificationsRepository = /** @class */ (function () {
+    function NotificationsRepository() {
+    }
+    return NotificationsRepository;
+}());
+exports.NotificationsRepository = NotificationsRepository;
