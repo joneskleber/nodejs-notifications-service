1c856f213a387e52b6c8bba47c790e52bd767e74
Jones Kleber <visao@visao-ol.com.br>
2022-12-17 17:06:37 -0300

    Ignite Labs|Nodejs - Microservices Nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,13 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http/http.modules';
 import { MessagingModule } from '@infra/messaging/messaging.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
-    HttpModule,
-    DatabaseModule,
-    MessagingModule,
-  ],
+  imports: [HttpModule, DatabaseModule, MessagingModule],
 })
 export class AppModule {}
+
+// ConfigModule.forRoot({
+//    isGlobal: true,
+//    envFilePath: '.env',
+//  }),
--- src/infra/messaging/kafka/kafka-consumer.service.ts
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { ServerKafka } from '@nestjs/microservices';
 
-import { ConfigService } from '@nestjs/config';
+// import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class KafkaConsumerService
@@ -9,21 +9,30 @@ export class KafkaConsumerService
   implements OnModuleDestroy
 {
   constructor() {
-    const config = new ConfigService();
-    const kafka_username = config.get<string>('KAFKA_USERNAME') || 'undefined';
-    const kafka_password = config.get<string>('KAFKA_PASSWORD') || 'undefined';
-    const kafka_clientId = config.get<string>('KAFKA_CLIENTID') || 'undefined';
-    const kafka_brokers = config.get<string>('KAFKA_BROKERS') || 'undefined';
+    //  const config = new ConfigService();
+    //  const kafka_username = config.get<string>('KAFKA_USERNAME') || 'undefined';
+    //  const kafka_password = config.get<string>('KAFKA_PASSWORD') || 'undefined';
+    //  const kafka_clientId = config.get<string>('KAFKA_CLIENTID') || 'undefined';
+    //  const kafka_brokers = config.get<string>('KAFKA_BROKERS') || 'undefined';
+
+    //  KAFKA_USERNAME =
+    //  KAFKA_PASSWORD =
+    //  KAFKA_CLIENTID =
+    //  KAFKA_BROKERS  =
 
     super({
       client: {
-        clientId: kafka_clientId,
-        brokers: [`${kafka_brokers}`],
+        clientId: 'notifications',
+        brokers: ['pretty-jackal-5748-us1-kafka.upstash.io:9092'],
         sasl: {
           mechanism: 'scram-sha-256',
-          username: kafka_username,
-          password: kafka_password,
+          username:
+            'cHJldHR5LWphY2thbC01NzQ4JL473RvTXEoFNXQ1t4UVm1VBfsU-5WSXFbPARKE',
+
+          password:
+            '6s8j5t3MfL0_Tc1biow0XJpAGgibxtRIDnjGJchNMwfU7P49tzK3PJufpCIN8cBNXHMCkA==',
         },
+        ssl: true,
       },
     });
   }
